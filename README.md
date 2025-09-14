d03357dd3679e68bb80824de95b13a20b342d619
Yang <yang@mac.home>
2025-09-13 22:32:22 -0400

    feat(tools): add basic tools for whole project

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# user-service-express
+# User Service Express
+
+## Description
+
+User Service is a simple microservice built with Node.js and Express. This service provides user management functionalities, including user registration, authentication, and profile management.
+
+## Features
+
+- User registration
+- User authentication
+- Profile management
+- Built with TypeScript for type safety
+- ESLint and Prettier for code quality and formatting
+- Husky for Git hooks
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js (version 14 or higher)
+- pnpm (package manager)
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/yourusername/user-service-express.git
+   cd user-service-express
+   ```
+
+2. Install dependencies:
+   ```bash
+   pnpm install
+   ```
+
+### Running the Application
+
+To start the application in development mode, run:
+
+```bash
+pnpm run dev
+```
+
+### Linting and Formatting
+
+To lint the code, run:
+
+```bash
+pnpm run lint
+```
+
+To format the code using Prettier, run:
+
+```bash
+pnpm run format:fix
+```
+
+## Author
+
+Sheng-Lin Yang
---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+// eslint.config.js
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier/flat";
+import pluginImport from "eslint-plugin-import";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  // eslintRecommended,
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,ts}"], // Specify the file scope where the configuration takes effect
+
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        browser: "readonly",
+        console: "readonly",
+      },
+    },
+
+    // Add the import plugin
+    plugins: {
+      import: pluginImport,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...prettierConfig.rules,
+      "import/order": [
+        "error",
+        {
+          groups: [["builtin", "external"], "internal", ["sibling", "parent"], "index"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true }, // Sort alphabetically
+        },
+      ],
+      "no-console": "off", // Allow use of console
+      "prettier/prettier": ["error", { endOfLine: "auto" }], // Enable Prettier rules as errors
+    },
+  },
+];
